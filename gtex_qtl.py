f386f56a6697564ac3d6f615b5d19bb70cafbadf
Étienne Morice <pro.emorice@mail.com>
2022-09-22 18:02:05 +0200

    feat(gtex_qtl): part of the downloadable resources

--- gtex_qtl.py
+++ gtex_qtl.py
@@ -3,8 +3,13 @@ GTEx QTL calling pipeline
 """
 
 import os
+import urllib.request
+import shutil
 
+import galp
 import wdl_galp
+
+pbl = galp.StepSet()
 
 def broad_wdl(path):
     """
@@ -18,7 +23,45 @@ def broad_wdl(path):
 
     return os.path.join(self_dir, 'gtex-pipeline', path)
 
+# download public expression files
+@pbl.step(vtag='0.2: return path')
+def urlretrieve(url, _galp):
+    """
+    Download file from url and check it in the store
+    """
+    to_path = _galp.new_path()
+    with urllib.request.urlopen(url) as in_fd:
+        with open(to_path, 'wb') as out_fd:
+            shutil.copyfileobj(in_fd, out_fd)
+    return to_path
+
+_GTEX_BASE_URL = 'https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/'
+
+wb_tpm = urlretrieve(_GTEX_BASE_URL +
+        'gene_tpm/gene_tpm_2017-06-05_v8_whole_blood.gct.gz')
+
+wb_counts = urlretrieve(_GTEX_BASE_URL +
+        'gene_reads/gene_reads_2017-06-05_v8_whole_blood.gct.gz')
+
+_GTEX_GENE_MODEL_URL = (
+    'https://personal.broadinstitute.org/francois/topmed/'
+    'gencode.v26.GRCh38.ERCC.genes.gtf.gz'
+    )
+
+gene_model = urlretrieve(_GTEX_GENE_MODEL_URL)
+
+# various normalization steps
+prepared_expression = wdl_galp.run(broad_wdl('qtl/eqtl_prepare_expression.wdl'),
+        **{
+            'eqtl_prepare_expression.tpm_gct': wb_tpm,
+            'eqtl_prepare_expression.counts_gct': wb_counts,
+            'eqtl_prepare_expression.annotation_gtf': gene_model,
+            }
+        )
+
 # run the fastqtl tool through WDL
-fastqtl = wdl_galp.run(broad_wdl('qtl/fastqtl.wdl'))
+fastqtl = wdl_galp.run(broad_wdl('qtl/fastqtl.wdl'),
+        expression_bed=prepared_expression['expression_bed']
+        )
 
 default_target = fastqtl

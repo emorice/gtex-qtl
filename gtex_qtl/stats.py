cb0825c38f954024860f0cfef4053a6a39fd5a67
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2022-11-18 19:09:42 +0100

    fix(stats): add intercept to genotype regression

--- gtex_qtl/stats.py
+++ gtex_qtl/stats.py
@@ -32,11 +32,19 @@ def regress_missing(data_bs, valid_bs, covariates_cs):
     """
     Variant of :func:`regress` that takes a validity mask on data. Invalid cells
     of `data_bs` must be zeroed.
-    """
 
+    This is **not** equivalent to regressing individually each vector of data
+    after filtering samples, as this would require solving a linear system with
+    a different set of samples for each one.
+    """
+    # Add the intercept pseudo-covariate
+    covariates_cs = np.vstack((np.ones(data_bs.shape[-1]), covariates_cs))
+
+    # Compute only one covariate variance covariance estimator, with all samples
     _n_vars, n_samples = covariates_cs.shape
     cov_cc = (covariates_cs @ covariates_cs.T) / n_samples
 
+    # Compute the individual cov-gt covariances with variable samples
     cov_cb = (covariates_cs @ data_bs.T) / np.sum(valid_bs, -1)
 
     weights_cb = np.linalg.inv(cov_cc) @ cov_cb

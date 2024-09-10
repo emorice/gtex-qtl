1d9761a8b9e430419fa54fab7524eb15bb3c577e
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2024-09-10 19:28:07 +0200

    feat(plots): pub-repro fig

--- gtex_qtl/plots.py
+++ gtex_qtl/plots.py
@@ -144,24 +144,41 @@ def pairs_pval_cdf(all_pairs_pvals):
             }
         )
 
-def vs_egenes(all_egenes_ic: dict[str, pd.DataFrame]) -> go.Figure:
+def vs_egenes_published_repro(all_egenes_perm: dict[str, pd.DataFrame]) -> go.Figure:
+    """
+    Plot number of eGenes
+    """
+    return vs_egenes(all_egenes_perm, (
+        ('published', 'Published'),
+        ('reproduction', 'Reproduction')
+        ))
+def vs_egenes_peer_cmk(all_egenes_ic: dict[str, pd.DataFrame]) -> go.Figure:
+    """
+    Plot number of eGenes
+    """
+    return vs_egenes(all_egenes_ic, (
+        ('reimplementation', 'PEER'),
+        ('cmk', 'cmk')
+        ))
+
+def vs_egenes(all_egenes: dict[str, pd.DataFrame], alts) -> go.Figure:
     """
     Plot number of eGenes
     """
     minexp = -5
     egenes = pd.DataFrame({
-        name: [np.sum(all_egenes_ic[ppl]['pval_beta'] < 10**k) for k in
+        name: [np.sum(all_egenes[name]['pval_beta'] < 10**k) for k in
                range(minexp, 0)]
-        for name, ppl in {'peer': 'reimplementation', 'cmk': 'cmk'}.items()
+        for name, _ in alts
     })
 
     # This needs to be tuned somewhat manually for best effect
-    xmin, xmax = egenes['peer'].iloc[0] - 800, egenes['cmk'].iloc[-1] + 400
+    xmin, xmax = egenes[alts[0][0]].iloc[0] - 800, egenes[alts[1][0]].iloc[-1] + 400
 
     return go.Figure([
         go.Scatter({
-            'x': egenes['peer'],
-            'y': egenes['cmk'],
+            'x': egenes[alts[0][0]],
+            'y': egenes[alts[1][0]],
             'text': [f'p < 10<sup>{k}</sup>' for k in range(minexp, 0)],
             'mode': 'lines+markers+text',
             'textposition': 'top left',
@@ -175,8 +192,12 @@ def vs_egenes(all_egenes_ic: dict[str, pd.DataFrame]) -> go.Figure:
             'showlegend': False,
         })
     ], {
-        'xaxis': {'range': [xmin, xmax], 'title': 'Genes with significant eQTL (PEER)'},
-        'yaxis': {'range': [xmin, xmax], 'title': 'Genes with significant eQTL (CMK)'}
+        'xaxis': {'range': [xmin, xmax],
+            'title': f'Genes with significant eQTL ({alts[0][1]})'
+            },
+        'yaxis': {'range': [xmin, xmax],
+        'title': f'Genes with significant eQTL ({alts[1][1]})'
+        }
     })
 
 def export_all_plots(store: str, output: str) -> None:
@@ -186,9 +207,14 @@ def export_all_plots(store: str, output: str) -> None:
     os.makedirs(output, exist_ok=True)
 
     all_egenes_ic = galp.run(gtex_qtl.all_egenes_ic, store=store)
+    all_egenes_perm = galp.run(gtex_qtl.all_egenes_perm, store=store)
 
-    vs_egenes(all_egenes_ic).write_image(
+    vs_egenes_peer_cmk(all_egenes_ic).write_image(
             os.path.join(output, 'egenes_peer_cmk.svg')
+            )
+
+    vs_egenes_published_repro(all_egenes_perm).write_image(
+            os.path.join(output, 'egenes_pub_repro.svg')
             )
 
 def main():
